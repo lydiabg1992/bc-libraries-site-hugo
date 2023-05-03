7d552be30df0aa412297e8c43508273777ed9de0
Lydia B-G <lburragegoodwin@gmail.com>
2023-05-03 13:01:54 -0400

    latest 5/3

--- themes/BC/assets/js/new-items-carousel.js
+++ themes/BC/assets/js/new-items-carousel.js
@@ -7,14 +7,14 @@ $('.responsive').slick({
     dots: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 5,
+    slidesToScroll: 2,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 2,
           infinite: true,
           dots: true
         }
